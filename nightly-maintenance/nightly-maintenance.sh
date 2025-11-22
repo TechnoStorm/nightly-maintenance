140760d0d963d211e2d67348ad88c2c5472a0f76
Techno <mail@mail.com>
2025-11-22 23:28:18 +0500

    Создан maintenance.sh

---
+++ nightly-maintenance/nightly-maintenance.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# Скрипт ночного техобслуживания NAS
+
+# принудительно прерываем скрипт при ошибках, и неинициализированных переменных
+set -euo pipefail
+
+
+# читаем конфиг
+source ./config.sh
+
+
+# функция логгирования
+log() {
+
+    local msg="[$(date '+%F %T')] $*"
+    echo "$msg" >> "$LOG_FILE"
+    echo "$msg"
+}
+
+
+# функция обрезки лога
+# (удаляются все строки, кроме последних $MAX_LOG_LINES)
+trim_log() {
+    if [[ -f "$LOG_FILE" ]]; then
+
+        # создаём временный файл лога, с уникальным именем
+        local tmp_file="${LOG_FILE}.tmp.$$"
+
+        if tail -n "$MAX_LOG_LINES" "$LOG_FILE" > "$tmp_file"; then
+            mv -f "$tmp_file" "$LOG_FILE"
+        else
+            rm -f "$tmp_file"
+            log "ОШИБКА: Обрезка лога не удалась"
+
+            # не прерываем скрипт через "exit 1",
+            # так-как функция всё равно выполняется в смом конце
+        fi
+    fi
+}
