7f4feb603ab5d5a1453e59bf39dd8e228386464a
Techno <mail@mail.com>
2025-11-23 22:28:51 +0500

    Добавлены параметры в config.sh
    - GITEA_DIR
    - GITEA_BIN_FILE
    - GITEA_CONFIG_FILE
    - GITEA_DUMP_DIR
    
    + Уточнения в комментариях

--- nightly-maintenance/config.sh
+++ nightly-maintenance/config.sh
@@ -6,8 +6,9 @@
 # Параметры логгирования
 #########################
 
-LOG_DIR="/home/user/sync/nas-backup/"
-LOG_FILE="$LOG_DIR/"
+# УКАЗЫВАТЬ КАТАЛОГИ БЕЗ КОНЕЧНОГО СЛЕША!
+LOG_DIR="/home/user/sync/gitea-backup"
+LOG_FILE="$LOG_DIR"
 MAX_LOG_LINES=200
 
 
@@ -15,6 +16,12 @@ MAX_LOG_LINES=200
 # Параметры Gitea
 ##################
 
+# УКАЗЫВАТЬ КАТАЛОГИ БЕЗ КОНЕЧНОГО СЛЕША!
+GITEA_DIR="/var/lib/gitea"
+GITEA_BIN_FILE="/usr/local/bin/gitea"
+GITEA_CONFIG_FILE="/etc/gitea/app.ini"
 GITEA_DB_FILE="/var/lib/gitea/gitea.db"
 GITEA_GIT_DIR="/var/lib/gitea/git"
 GITEA_LFS_DIR="/var/lib/gitea/lfs"
+GITEA_DUMP_DIR="/home/user/sync/gitea-backup"
+GITEA_DUMP_NAME="gitea-dump"$(date +"%F_%H_%M_%S")".zip"
