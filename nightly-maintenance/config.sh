53d30162a9f280baeb6dfc61520bd9ede77adbbe
Techno <mail@mail.com>
2025-11-23 21:04:41 +0500

    Добавлены параметры Gitea

--- nightly-maintenance/config.sh
+++ nightly-maintenance/config.sh
@@ -9,3 +9,12 @@
 LOG_DIR="/home/user/sync/nas-backup/"
 LOF_FILE="$LOG_DIR/"
 MAX_LOG_LINES=200
+
+
+##################
+# Параметры Gitea
+##################
+
+GITEA_DB_FILE="/var/lib/gitea/gitea.db"
+GITEA_GIT_DIR="/var/lib/gitea/git"
+GITEA_LFS_DIR="/var/lib/gitea/lfs"
