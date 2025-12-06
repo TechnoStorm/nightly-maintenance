6a94b341e869f96a89fcee529bf0420383089970
Techno <mail@mail.com>
2025-12-06 19:14:57 +0500

    Переименован каталог для дампов и лога

--- nightly-maintenance/config.sh
+++ nightly-maintenance/config.sh
@@ -11,7 +11,7 @@ HDD_MOUNT_POINT="/VAR/HDD"
 #########################
 
 # УКАЗЫВАТЬ КАТАЛОГИ БЕЗ КОНЕЧНОГО СЛЕША!
-LOG_DIR="/home/user/sync/gitea-backup"
+LOG_DIR="/home/user/sync/gitea-dumps"
 LOG_FILE="nightly_maintenance.log"
 MAX_LOG_LINES=200
 
@@ -28,5 +28,5 @@ GITEA_DB_FILE="/var/lib/gitea/gitea.db"
 GITEA_GIT_DIR="/var/lib/gitea/git"
 GITEA_LFS_DIR="/var/lib/gitea/lfs"
 GITEA_LFS_BACKUP_DIR="/var/hdd/git-lfs"
-GITEA_DUMP_DIR="/home/user/sync/gitea-backup"
+GITEA_DUMP_DIR="/home/user/sync/gitea-dumps"
 GITEA_DUMP_NAME="gitea-dump" # только имя (без расширения!), таймстамп добавится автоматом
