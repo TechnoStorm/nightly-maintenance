a36d7486a90067a3caeda59abb2d22a11c556cef
Techno <mail@mail.com>
2025-12-06 19:42:03 +0500

    Правка параметров конфига

--- nightly-maintenance/config.sh
+++ nightly-maintenance/config.sh
@@ -3,7 +3,7 @@
 # Файл конфигурации для nightly-maintenance.sh
 
 
-HDD_MOUNT_POINT="/VAR/HDD"
+HDD_MOUNT_POINT="/var/hdd"
 
 
 #########################
@@ -27,6 +27,6 @@ GITEA_CONFIG_FILE="/etc/gitea/app.ini"
 GITEA_DB_FILE="/var/lib/gitea/gitea.db"
 GITEA_GIT_DIR="/var/lib/gitea/git"
 GITEA_LFS_DIR="/var/lib/gitea/lfs"
-GITEA_LFS_BACKUP_DIR="/var/hdd/git-lfs"
+GITEA_LFS_BACKUP_DIR="git-lfs"
 GITEA_DUMP_DIR="/home/user/sync/gitea-dumps"
 GITEA_DUMP_NAME="gitea-dump" # только имя (без расширения!), таймстамп добавится автоматом
