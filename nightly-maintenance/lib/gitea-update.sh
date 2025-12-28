827f6dd27a1ba2766e9428c0971c018fb3c9b8d4
Techno <mail@mail.com>
2025-12-28 20:42:23 +0500

    Добавлены проверки текущей и свежей версии gitea

--- nightly-maintenance/lib/gitea-update.sh
+++ nightly-maintenance/lib/gitea-update.sh
@@ -12,14 +12,36 @@ set -euo pipefail
 
 log "Проверка наличия обновлений Gitea..."
 
-# Узнаём текущую версию бинарника Gitea
-GITEA_CURRENT_VERSION="$("$GITEA_BIN_FILE" --version | awk '{print $3}')" || fail "\"gitea --version\" завершилась с ошибкой"
+log "Определение текущей версии Gitea..."
 
+# Узнаём версию текущего бинарника Gitea
+GITEA_CURRENT_VERSION="$("$GITEA_BIN_FILE" --version | awk '{print $3}')"
 
-if [ -n "$GITEA_CURRENT_VERSION" ]; then
+if [[ -n "$GITEA_CURRENT_VERSION" ]]; then
     log "Текущая версия Gitea: $GITEA_CURRENT_VERSION"
 else
     fail "\"gitea --version\" вернула пустой результат"
 fi
 
 
+log "Проверка наличия обновлений Gitea..."
+
+log "Получение JSON-файла latest-версии Gitea..."
+
+JSON=$(curl -fsSL "https://api.github.com/repos/go-gitea/gitea/releases/latest")
+
+log "Парсинг JSON-файла..."
+
+# Парсим тег версии и проверяем формат "x.x.x"
+GITEA_LATEST_VERSION=$(
+    echo "$JSON" | jq -r '
+    .tag_name
+    | ltrimstr("v")
+    | select(test("^[0-9]+\\.[0-9]+\\.[0-9]+$"))
+    '
+)
+
+# Проверка наличия результата парсинга
+[[ -n "$GITEA_LATEST_VERSION" ]] || fail "Парсинг JSON-файла вернул пустое значение"
+
+echo "$GITEA_LATEST_VERSION"
