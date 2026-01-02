1b4c019a42205d2c97510f4627ce458529ed148f
Techno <mail@mail.com>
2026-01-02 22:03:21 +0500

    Удалён лишний параметр конфига

--- nightly-maintenance/config.sh
+++ nightly-maintenance/config.sh
@@ -29,6 +29,5 @@ GITEA_DB_FILE="/var/lib/gitea/gitea.db"
 GITEA_GIT_DIR="/srv/repos/git" # исходные репозитории
 GITEA_LFS_DIR="/srv/repos/lfs" # исходное LFS-хранилище
 GITEA_LFS_BACKUP_DIR="/var/hdd/backup/git-lfs" # директория резервной копии LFS-хранилища
-GITEA_BIN_BACKUP_DIR="/srv/sync/gitea-backup" # директория хранения предыдущей версии бинарника Gitea
 GITEA_DUMP_DIR="/srv/sync/gitea-backup"
 GITEA_DUMP_NAME="gitea-dump" # только имя (без расширения!), таймстамп добавится автоматом
