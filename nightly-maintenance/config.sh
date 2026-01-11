8123422ebb8a28d0cab6205b6c85b8b5d4ff6083
Techno <mail@mail.com>
2026-01-11 21:18:34 +0500

    Update config.sh
    Правка параметров конфигурации

--- nightly-maintenance/config.sh
+++ nightly-maintenance/config.sh
@@ -3,7 +3,7 @@
 # Файл конфигурации для nightly-maintenance.sh
 
 
-HDD_MOUNT_POINT="/var/hdd" # точка монтирования HDD для бэкапов
+HDD_MOUNT_POINT="/srv/hdd" # точка монтирования HDD для бэкапов
 
 
 #########################
@@ -26,10 +26,12 @@ GITEA_SYSTEM="linux-arm64"
 GITEA_USER="gitea"
 GITEA_BIN_FILE="/usr/local/bin/gitea"
 GITEA_CONFIG_FILE="/etc/gitea/app.ini"
-GITEA_DB_FILE="/var/lib/gitea/gitea.db"
+GITEA_DB_FILE="/var/lib/gitea/data/gitea.db"
 GITEA_GIT_DIR="/srv/repos/git" # исходные репозитории
 GITEA_LFS_DIR="/srv/repos/lfs" # исходное LFS-хранилище
-GITEA_LFS_BACKUP_DIR="/var/hdd/backup/git-lfs" # директория резервной копии LFS-хранилища
+GITEA_GIT_BACKUP_DIR="/srv/hdd/backup/git" # директория резервной копии Git-репозиториев
+GITEA_LFS_BACKUP_DIR="/srv/hdd/backup/lfs" # директория резервной копии LFS-хранилища
+GITEA_LFS_BACKUP_CLEAR="false" # флаг очистки LFS-бэкапа от удалённых из оригинального LFS-хранилища файлов
 GITEA_DUMP_DIR="/srv/sync/gitea-backup"
 GITEA_DUMP_NAME="gitea-dump" # только имя (без расширения!), таймстамп добавится автоматом
 GITEA_DUMP_CHOWN="user:user"
