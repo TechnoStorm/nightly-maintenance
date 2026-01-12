f5ae71f722bc1b0eb456d181e9e6533f8c6423d9
Techno <mail@mail.com>
2026-01-12 21:19:16 +0500

    Удалён ненужный флаг очистки LFS

--- nightly-maintenance/config.sh
+++ nightly-maintenance/config.sh
@@ -31,7 +31,6 @@ GITEA_GIT_DIR="/srv/repos/git" # исходные репозитории
 GITEA_LFS_DIR="/srv/repos/lfs" # исходное LFS-хранилище
 GITEA_GIT_BACKUP_DIR="/srv/hdd/backup/git" # директория резервной копии Git-репозиториев
 GITEA_LFS_BACKUP_DIR="/srv/hdd/backup/lfs" # директория резервной копии LFS-хранилища
-GITEA_LFS_BACKUP_CLEAR="false" # флаг очистки LFS-бэкапа от удалённых из оригинального LFS-хранилища файлов
 GITEA_DUMP_DIR="/srv/sync/gitea-backup"
 GITEA_DUMP_NAME="gitea-dump" # только имя (без расширения!), таймстамп добавится автоматом
 GITEA_DUMP_CHOWN="user:user"
