3fc4b13ec4df642e5f16315685ce00fdacec0f4d
Techno <mail@mail.com>
2026-01-25 00:20:13 +0500

    Доработка cleaner

--- nightly-maintenance/cleaner.sh
+++ nightly-maintenance/cleaner.sh
@@ -17,14 +17,80 @@ BASE_DIR="$(dirname "$(readlink -f "$0")")"
 # Читаем конфиг
 source "$BASE_DIR/config.sh"
 
+# Проверяем: смонтирован-ли HDD на точку монтирования?
+mountpoint -q "$HDD_MOUNT_POINT" || {
+    echo "[ERROR]: $HDD_MOUNT_POINT не является точкой монтирования"
+    exit 1
+}
+
+# С помощью файла-маркера проверяем: точно-ли смонтирован правильный диск?
+[[ -f "$HDD_MOUNT_POINT"/nas-hdd-marker ]] || {
+    echo "[ERROR]: Файл-маркер (\"nas-hdd-marker\") отсутствует на $HDD_MOUNT_POINT"
+    exit 1
+}
+
 
 ###############
 # Меню скрипта
 ###############
 
+trash_detected=false # флаг наличия мусора
+
+# Функция вычисления разницы по весу
+size_diff() {
+    local src="$1"
+    local bcp="$2"
+    local type="$3"
+
+    if [[ ! -d "$src" ]]; then
+        echo "[ERROR]: Исходная директория не найдена: $src"
+        exit 1
+    fi
+
+    if [[ ! -d "$bcp" ]]; then
+        echo "[ERROR]: Директория бэкапа не найдена: $bcp"
+        exit 1
+    fi
+
+    # Узнаём размеры исходника и бэкапа
+    local src_size bcp_size size_diff_result
+    src_size=$(du -sb "$src" | awk '{print $1}')
+    bcp_size=$(du -sb "$bcp" | awk '{print $1}')
+
+
+    # Выводим размеры исходника и бэкапа
+    echo "$type-исходник   : $src_size байт ($(numfmt --to=iec $src_size))"
+    echo "$type-бэкап      : $bcp_size байт ($(numfmt --to=iec $bcp_size))"
+
+    # Вычисляем: есть-ли мусор?
+    size_diff_result=$(( bcp_size - src_size ))
+
+    if (( size_diff_result > 0 )); then
+
+        trash_detected=true
+        echo "Мусор: $size_diff_result байт ($(numfmt --to=iec $size_diff_result))"
+    else
+        echo "Мусор : Нет"
+    fi
+}
+
 clear
 
+echo
+
 echo "Скрипт очистки бэкапов NAS"
+echo
+
+size_diff "$GITEA_GIT_DIR" "$GITEA_GIT_BACKUP_DIR" "Git"
+echo
+size_diff "$GITEA_LFS_DIR" "$GITEA_LFS_BACKUP_DIR" "LFS"
+
+if [[ $trash_detected == false ]]; then
+    echo
+    echo "Очистка не требуется"
+    exit 0
+fi
+
 echo
 echo "Выберите цель очистки:"
 echo "1 - Git-репозитории"
@@ -79,9 +145,15 @@ clean() {
         exit 1
     fi
 
-    echo "Начат процесс очиски: $bcp"
+    echo "Начат процесс очистки: $bcp"
 
-    if ! rsync -aH  --numeric-ids --delete --stats --dry-run "$src/" "$bcp/"; then
+    # Перестраховочная проверка аргументов
+    [[ -n "$src" && -n "$bcp" && "$bcp" != "/" ]] || {
+        echo "[ERROR]: Неправильные аргументы функции"
+        exit 1
+    }
+
+    if ! rsync -aH --existing --delete --stats --dry-run "$src/" "$bcp/"; then
         echo "[ERROR]: Ошибка выполнения Rsync"
         exit 1
     fi
