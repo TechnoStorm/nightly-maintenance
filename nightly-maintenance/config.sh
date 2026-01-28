9bcbeddc136ea1f3a36666cfc50df4de89a0c144
Techno <mail@mail.com>
2026-01-29 00:32:18 +0500

    Правка директории лога и дампов Gitea

--- nightly-maintenance/config.sh
+++ nightly-maintenance/config.sh
@@ -12,7 +12,7 @@ HDD_MOUNT_POINT="/srv/hdd" # точка монтирования HDD для бэ
 #########################
 
 # УКАЗЫВАТЬ КАТАЛОГИ БЕЗ КОНЕЧНОГО СЛЕША!
-LOG_FILE="/srv/sync/gitea-backup/nightly-maintenance.log"
+LOG_FILE="/srv/sync/gitea-backups/nightly-maintenance.log"
 MAX_LOG_LINES=5000
 LOG_CHOWN="user:user"
 LOG_CHMOD=640
@@ -41,7 +41,7 @@ GITEA_GIT_DIR="/srv/repos/git" # исходные репозитории
 GITEA_LFS_DIR="/srv/repos/lfs" # исходное LFS-хранилище
 GITEA_GIT_BACKUP_DIR="/srv/hdd/backup/git" # директория резервной копии Git-репозиториев
 GITEA_LFS_BACKUP_DIR="/srv/hdd/backup/lfs" # директория резервной копии LFS-хранилища
-GITEA_DUMP_DIR="/srv/sync/gitea-backup" # директория для дампов Gitea
+GITEA_DUMP_DIR="/srv/sync/gitea-backups" # директория для дампов Gitea
 GITEA_DUMP_NAME="gitea-dump" # только имя (без расширения!), таймстамп добавится автоматом
 GITEA_DUMP_CHOWN="user:user"
 GITEA_DUMP_CHMOD=660
